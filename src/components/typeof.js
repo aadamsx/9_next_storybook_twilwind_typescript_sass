63fff8453440c6b9901deaaa00bb5382fa00d6d5
aadamsx <aadams99@gmail.com>
2022-04-15 10:21:16 -0500

    updates

---
+++ src/components/typeof.js
@@ -0,0 +1,6 @@
+// What does the following code evaluate to?
+typeof typeof 0;
+// Answer
+// It evaluates to "string".
+
+// typeof 0 evaluates to the string "number" and therefore typeof "number" evaluates to "string".
